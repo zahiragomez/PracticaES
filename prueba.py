bd17fcd4488f13446486682c6c603e939a36d286
ainhoadediego <147641031+ainhoadediego@users.noreply.github.com>
2023-10-18 18:48:48 +0200

    Add files via upload

---
+++ prueba.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+ruta_del_archivo_csv = r"C:\Users\lovea\Downloads\housing.csv"
+path_csv = pd.read_csv(ruta_del_archivo_csv)
+#print(path_csv.head())
+
+ruta_del_archivo_excel = r"C:\Users\lovea\Downloads\housing.xlsx"
+path_excel = pd.read_excel(ruta_del_archivo_excel)
+print(path_excel.head())
