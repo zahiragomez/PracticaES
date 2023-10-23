affdd7551b03b96bdb23bce32bfb41597c61b37b
zahiragomez <zahira.gomez@udc.es>
2023-10-23 23:21:33 +0200

    try para  facilitar cambios

--- Practica/prueba.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-ruta_del_archivo_csv = r"c:\Users\User\Downloads\housing.csv"
-path_csv = pd.read_csv(ruta_del_archivo_csv)
-print(path_csv.head())
-
-ruta_del_archivo_excel = r"c:\Users\User\Downloads\housing.xlsx"
-path_excel = pd.read_excel(ruta_del_archivo_excel)
-print(path_excel.head())
