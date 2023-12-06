c7c6bcb39a0d39fd030eac087f6a1370217b75f4
ainhoadediego <147641031+ainhoadediego@users.noreply.github.com>
2023-12-06 14:17:36 +0100

    Muestra paso a paso y solo 1 grafica

--- Practica/main_GUI.py
+++ Practica/main_GUI.py
@@ -1,3 +1,4 @@
+
 import tkinter as tk
 from tkinter import filedialog
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
@@ -14,6 +15,7 @@ class PantallaPrincipal(tk.Frame):
         self.col_y = None
         self.fig1 = None
         self.fig2 = None
+        self.canvas_regresion = None
 
         self.configure(background="light blue")
         self.controller = controller
@@ -23,39 +25,35 @@ class PantallaPrincipal(tk.Frame):
         self.variables_seleccionadas_x = []
         self.variables_seleccionadas_y = []
 
-        # Crear el frame para la ruta del archivo y botón de examinar
-        frame_archivo_seleccionado = tk.Frame(self, bg="light blue")
-        frame_archivo_seleccionado.pack(side=tk.TOP, padx=10, pady=10)
+        # Crear el frame para la selección de archivo
+        self.frame_archivo_seleccionado = tk.Frame(self, bg="light blue")
+        self.frame_archivo_seleccionado.pack(side=tk.TOP, padx=10, pady=10)
 
         # Indicaciones
-        indicaciones_label = tk.Label(
-            frame_archivo_seleccionado,
+        self.indicaciones_label = tk.Label(
+            self.frame_archivo_seleccionado,
             text="(Seleccione el archivo dándole a examinar)",
             justify=tk.LEFT,
             font=("Comfortaa", 12),
             bg="white",
             fg="black",
         )
-        indicaciones_label.grid(
-            row=0, column=0, padx=(10, 10), pady=10, sticky=tk.W
-        )
+        self.indicaciones_label.grid(row=0, columnspan=2, padx=(10, 10), pady=10, sticky=tk.W)
 
         # Ruta del archivo
-        ruta_label = tk.Label(
-            frame_archivo_seleccionado,
+        self.ruta_label = tk.Label(
+            self.frame_archivo_seleccionado,
             textvariable=self.ruta_seleccionada,
             justify=tk.CENTER,
             font=("Comfortaa", 12),
             bg="white",
             fg="black",
         )
-        ruta_label.grid(
-            row=1, column=0, padx=(10, 10), pady=10, sticky=tk.W
-        )
+        self.ruta_label.grid(row=1, columnspan=2, padx=(10, 10), pady=10, sticky=tk.W)
 
         # Botón Examinar
-        boton_examinar = tk.Button(
-            frame_archivo_seleccionado,
+        self.boton_examinar = tk.Button(
+            self.frame_archivo_seleccionado,
             text="Examinar",
             command=self.seleccionar_archivo,
             justify=tk.RIGHT,
@@ -63,73 +61,13 @@ class PantallaPrincipal(tk.Frame):
             bg="white",
             fg="black",
         )
-        boton_examinar.grid(
-            row=1, column=1, padx=(10, 10), pady=10, sticky=tk.W
-        )
+        self.boton_examinar.grid(row=1, column=2, padx=(10, 10), pady=10, sticky=tk.W)
 
-        # Crear la lista de columnas X
-        self.listbox_x = tk.Listbox(frame_archivo_seleccionado)
-        self.listbox_x.bind("<<ListboxSelect>>", self.cambia_columna_x)
-        self.listbox_x.grid(
-            row=2, column=0, padx=(10, 5), pady=10, sticky=tk.W
-        )
-
-        # Crear la lista de columnas Y
-        self.listbox_y = tk.Listbox(frame_archivo_seleccionado)
-        self.listbox_y.bind("<<ListboxSelect>>", self.cambia_columna_y)
-        self.listbox_y.grid(
-            row=2, column=1, padx=(5, 10), pady=10, sticky=tk.W
-        )
-
-        
-        # Etiqueta para la columna X seleccionada
-        self.etiqueta_x = tk.Label(
-            frame_archivo_seleccionado,  # Usa frame_archivo_seleccionado como el contenedor
-            text=f"Variable X seleccionada: {self.col_x}",
-            font=("Comfortaa", 12),
-            bg="light blue",
-            fg="black",
-        )
-        self.etiqueta_x.grid(row=3, column=0, padx=(10, 10), pady=10, sticky=tk.W)
-        # Etiqueta para la columna Y seleccionada
-        self.etiqueta_y = tk.Label(
-            frame_archivo_seleccionado,  # Usa frame_archivo_seleccionado como el contenedor
-            text=f"Variable Y seleccionada: {self.col_y}",
-            font=("Comfortaa", 12),
-            bg="light blue",
-            fg="black",
-        )
-        self.etiqueta_y.grid(row=3, column=1, padx=(10, 10), pady=10, sticky=tk.E)
-
-        # Botón Realizar Análisis
-        boton_analisis = tk.Button(
-            frame_archivo_seleccionado,
-            text="Realizar Análisis",
-            command=self.realizar_analisis,
-            justify=tk.RIGHT,
-            font=("Comfortaa", 12),
-            bg="white",
-            fg="black",
-        )   
-        boton_analisis.grid(row=4, column=0, columnspan=2, pady=10, sticky=tk.NSEW)
-
-        # Canvas para el gráfico 1
-        self.canvas1 = tk.Canvas(self, width=400, height=300, bg="white")
-        self.canvas1.pack(padx=10, pady=10, side=tk.LEFT)
-
-        # Canvas para el gráfico 2
-        self.canvas2 = tk.Canvas(self, width=400, height=300, bg="white")
-        self.canvas2.pack(padx=10, pady=10, side=tk.LEFT)
-
-        # Etiqueta para mostrar el RMSE
-        self.etiqueta_rmse = tk.Label(
-            self,
-            text="RMSE: ",
-            font=("Comfortaa", 12),
-            bg="light blue",
-            fg="black",
-        )
-        self.etiqueta_rmse.pack(side=tk.TOP, padx=10, pady=10, anchor=tk.W)
+        # Listas de selección de columnas (se crean vacías inicialmente)
+        self.listbox_x = None
+        self.listbox_y = None
+        self.etiqueta_x = None
+        self.etiqueta_y = None
 
     def seleccionar_archivo(self):
         self.ruta_archivo = filedialog.askopenfilename()
@@ -144,7 +82,6 @@ class PantallaPrincipal(tk.Frame):
             self.col_x = self.listbox_x.get(self.listbox_x.curselection())
             self.etiqueta_x.config(text=f"Variable X seleccionada: {self.col_x}")
 
-
     def cambia_columna_y(self, event):
         if self.listbox_y.curselection():
             self.col_y = self.listbox_y.get(self.listbox_y.curselection())
@@ -155,20 +92,51 @@ class PantallaPrincipal(tk.Frame):
             df = pd.read_csv(self.ruta_archivo)
             columnas = df.columns.tolist()
 
-            self.listbox_x.delete(0, tk.END)
-            self.listbox_y.delete(0, tk.END)
+            # Crear la lista de columnas X
+            self.listbox_x = tk.Listbox(self.frame_archivo_seleccionado)
+            self.listbox_x.bind("<<ListboxSelect>>", self.cambia_columna_x)
+            self.listbox_x.grid(row=2, column=0, padx=(10, 5), pady=10, sticky=tk.W)
+
+            # Crear la lista de columnas Y
+            self.listbox_y = tk.Listbox(self.frame_archivo_seleccionado)
+            self.listbox_y.bind("<<ListboxSelect>>", self.cambia_columna_y)
+            self.listbox_y.grid(row=2, column=1, padx=(5, 10), pady=10, sticky=tk.W)
 
             for columna in columnas:
                 self.listbox_x.insert(tk.END, columna)
                 self.listbox_y.insert(tk.END, columna)
 
             # Etiqueta para la columna X seleccionada
-            etiqueta_x_text = f"Variable X seleccionada: {self.col_x}"
-            self.etiqueta_x.config(text=etiqueta_x_text)
+            self.etiqueta_x = tk.Label(
+                self.frame_archivo_seleccionado,
+                text=f"Variable X seleccionada: {self.col_x}",
+                font=("Comfortaa", 12),
+                bg="light blue",
+                fg="black",
+            )
+            self.etiqueta_x.grid(row=3, column=0, padx=(10, 10), pady=10, sticky=tk.W)
 
             # Etiqueta para la columna Y seleccionada
-            etiqueta_y_text = f"Variable Y seleccionada: {self.col_y}"
-            self.etiqueta_y.config(text=etiqueta_y_text)
+            self.etiqueta_y = tk.Label(
+                self.frame_archivo_seleccionado,
+                text=f"Variable Y seleccionada: {self.col_y}",
+                font=("Comfortaa", 12),
+                bg="light blue",
+                fg="black",
+            )
+            self.etiqueta_y.grid(row=3, column=1, padx=(10, 10), pady=10, sticky=tk.E)
+
+            # Botón Crear Modelo
+            self.boton_modelo = tk.Button(
+                self.frame_archivo_seleccionado,
+                text="Crear Modelo",
+                command=self.realizar_analisis,
+                justify=tk.RIGHT,
+                font=("Comfortaa", 12),
+                bg="white",
+                fg="black",
+            )   
+            self.boton_modelo.grid(row=4, columnspan=3, pady=10, sticky=tk.NSEW)
 
         except pd.errors.EmptyDataError:
             print("Error: El archivo está vacío")
@@ -179,82 +147,60 @@ class PantallaPrincipal(tk.Frame):
         if self.col_x is not None and self.col_y is not None:
             modelo = self.ajustar_modelo(self.col_x, self.col_y)
             if modelo:
-                self.fig1, self.fig2 = self.test_modelos(self.col_x, self.col_y, modelo)
-                if self.fig1:
-                    self.actualizar_grafica(self.fig1, self.canvas1)
-                if self.fig2:
-                    self.actualizar_grafica(self.fig2, self.canvas2)
+                self.actualizar_recta_regresion(modelo)
 
                 # Calcular y mostrar RMSE
                 rmse = self.calcular_rmse(self.col_x, self.col_y, modelo)
-                self.etiqueta_rmse.config(text=f'RMSE: {rmse:.4f}')  # Actualiza la etiqueta con el valor del RMSE
+                #self.etiqueta_rmse.config(text=f'RMSE: {rmse:.4f}')  # Actualiza la etiqueta con el valor del RMSE
 
     def ajustar_modelo(self, col_x, col_y):
         data = pd.read_csv(self.ruta_archivo)
 
+        # Drop rows with any missing values in the specified columns
+        data.dropna(subset=[col_x, col_y], inplace=True)
+
         X = data[col_x]
-        X = sm.add_constant(X, prepend=True)
+        X_i = sm.add_constant(X, prepend=True)
         y = data[col_y]
 
-        modelo = sm.OLS(endog=y, exog=X).fit()
+        modelo = sm.OLS(endog=y, exog=X_i).fit()
         return modelo
 
-    def test_modelos(self, col_x, col_y, modelo):
-        data = pd.read_csv(self.ruta_archivo)
 
-        # Análisis y gráfico 1
-        fig1, ax1 = plt.subplots(figsize=(8, 6))
-        scatter1 = ax1.scatter(
-            x=data[col_x],
-            y=data[col_y],
+    def actualizar_recta_regresion(self, modelo):
+        data = pd.read_csv(self.ruta_archivo)
+        
+        fig, ax = plt.subplots(figsize=(8, 6))
+        scatter = ax.scatter(
+            x=data[self.col_x],
+            y=data[self.col_y],
             c="#FFA500",
             label="Datos de dispersión",
             marker="o",
         )
-        ax1.set_title(f'Distribución de {col_x} y {col_y}')
-        ax1.set_xlabel('Eje X')
-        ax1.set_ylabel('Eje Y')
-        ax1.legend(handles=[scatter1], loc='upper right')
-
-        # Otro análisis y gráfico 2
-        fig2, ax2 = plt.subplots(figsize=(10, 6))
-        scatter2 = ax2.scatter(
-            x=data[col_x],
-            y=data[col_y],
-            c="gray",
-            label="Datos Observados",
-            marker="o",
-            alpha=0.8,
-        )
-        ax2.plot(data[col_x], modelo.predict(exog=sm.add_constant(data[col_x], prepend=True)), linestyle='-', color='blue', label="OLS", linewidth=2)
-        ci = modelo.get_prediction(exog=sm.add_constant(data[col_x], prepend=True)).summary_frame(alpha=0.05)
-        ax2.fill_between(data[col_x], ci["mean_ci_lower"], ci["mean_ci_upper"], color='orange', alpha=0.1, label='95% CI')
-        ax2.set_xlabel('Variable Independiente')
-        ax2.set_ylabel('Variable Dependiente')
-        ax2.set_title('Gráfico del Modelo con Predicciones')
-        ax2.grid(True, linestyle='--', alpha=0.5)
-        ax2.set_facecolor('#F9F9F9')
-        ax2.legend()
-
-        return fig1, fig2
-
-    def actualizar_grafica(self, fig, canvas):
-        # Crear una nueva instancia de FigureCanvasTkAgg
-        canvas = FigureCanvasTkAgg(fig, master=canvas)
+        ax.set_title(f'Distribución de {self.col_x} y {self.col_y}')
+        ax.set_xlabel('Eje X')
+        ax.set_ylabel('Eje Y')
+        ax.legend(handles=[scatter], loc='upper right')
+        ax.plot(data[self.col_x], modelo.predict(exog=sm.add_constant(data[self.col_x], prepend=True)), linestyle='-', color='blue', label="OLS", linewidth=2)
+        ci = modelo.get_prediction(exog=sm.add_constant(data[self.col_x], prepend=True)).summary_frame(alpha=0.05)
+        ax.fill_between(data[self.col_x], ci["mean_ci_lower"], ci["mean_ci_upper"], color='orange', alpha=0.1, label='95% CI')
+        
+        self.canvas_regresion = tk.Canvas(self, width=400, height=300, bg="white")
+        self.canvas_regresion.pack(padx=10, pady=10)
+        
+        canvas = FigureCanvasTkAgg(fig, master=self.canvas_regresion)
         canvas.draw()
         canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=True)
 
     def calcular_rmse(self, col_x, col_y, modelo):
         data = pd.read_csv(self.ruta_archivo)
 
-        # Preparar datos para la predicción
         X = sm.add_constant(data[col_x], prepend=True)
         y_true = data[col_y]
 
-        # Predecir los valores
         y_pred = modelo.predict(exog=X)
 
-        # Calcular el RMSE
         rmse = mean_squared_error(y_true, y_pred, squared=False)
         return rmse
 
@@ -291,4 +237,3 @@ class Manager(tk.Tk):
 if __name__ == "__main__":
     app = Manager()
     app.mainloop()
-
